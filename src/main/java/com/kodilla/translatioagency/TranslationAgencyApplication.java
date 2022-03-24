5f9fbfbacf4c879eefc78a7e7faf39436a7125e7
kpikula <90643790+kpikula@users.noreply.github.com>
2022-03-24 18:25:51 +0100

    initial commit

---
+++ src/main/java/com/kodilla/translatioagency/TranslationAgencyApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.translatioagency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslationAgencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TranslationAgencyApplication.class, args);
+    }
+
+}
